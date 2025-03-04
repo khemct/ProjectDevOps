96d1cb18ed2603205e89342332b47f6e6a439e81
KhemCT <chadchapong11043@gmail.com>
2025-03-04 17:39:52 +0700

    add EXIT_ON_CLOSE to GameWindow.java
    for when we click close window program will stop running

--- main/src/main/java/com/example/main/GameWindow.java
+++ main/src/main/java/com/example/main/GameWindow.java
@@ -9,6 +9,7 @@ public class GameWindow {
         jFrame = new JFrame();
         jFrame.setSize(400, 400);
         jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 }
